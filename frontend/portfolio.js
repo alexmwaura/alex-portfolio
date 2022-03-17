18403cfe8d91530adc14201e3e26c8982c2382bf
alexmwaura <alexmwaura43@gmail.com>
2022-03-17 15:24:34 +0300

    final polished

---
+++ frontend/portfolio.js
@@ -0,0 +1,39 @@
+const portfolio = document.getElementById('portfolio')
+const details = document.getElementById('details')
+const submit_1 = document.getElementById('submit-portfolio-1')
+const submit_2 = document.getElementById('submit-portfolio-2')
+
+const submit_3 = document.getElementById('submit-portfolio-3')
+
+const submit_4 = document.getElementById('submit-portfolio-4')
+
+const submit_5 = document.getElementById('submit-portfolio-5')
+
+
+const project = document.getElementById('project_section')
+
+submit_1.addEventListener('click', (e) => {
+   
+    
+})
+
+submit_2.addEventListener('click', (e) => {
+    
+})
+
+submit_3.addEventListener('click', (e) => {
+    
+})
+
+submit_4.addEventListener('click', (e) => {
+    
+})
+
+submit_5.addEventListener('click', (e) => {
+    
+})
+
+
+function getProject() {
+
+}
